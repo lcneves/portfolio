4b26158304de0131eb6ad6d548c596418ad72a23
Lucas Neves <lcneves@gmail.com>
2016-02-11 20:03:43 +0200

    App menu and code cleanup

--- script/index-react.js
+++ script/index-react.js
@@ -1,119 +1,113 @@
-var PortfolioItemTag = React.createClass({
-  render: function() {
-    var tagArray = this.props.tags.map(function(tag, index) {
+(function () {
+  var PortfolioItemTag = React.createClass({
+    render: function() {
+      var tagArray = this.props.tags.map(function(tag, index) {
+        return (
+          <span key={index} className="tag">
+            {tag}
+          </span>
+        );
+      });
       return (
-        <span key={index} className="tag">
-          {tag}
+        <div className="tagsDiv">
+          {tagArray}
+        </div>
+      );
+    }
+  });
+
+  var PortfolioItem = React.createClass({
+    render: function() {
+      return (
+        <div className="portfolioItem" onClick={myPortfolio.openApp.bind(null, this.props.item.link)}>
+          <img src={'img/' + this.props.item.picture} />
+          <div className="portfolioItemName">
+            {this.props.item.name}
+          </div>
+        </div>
+      );
+    }
+  });
+
+  var PortfolioList = React.createClass({
+    render: function() {
+      var itemArray = this.props.data.map(function(item, index) {
+        return (
+          <PortfolioItem key={index} item={item} />
+        );
+      });
+      return (
+        <div className="portfolioFlexContainer">
+          {itemArray}
+        </div>
+      );
+    }
+  });
+
+  var frontendPortfolioArray = [
+    {
+      name: "Chocolate Calc",
+      picture: "Calculator.png",
+      description: "A calculator that looks like a chocolate bar!",
+      link: "calculator/",
+      technologies: ["CSS3", "JavaScript"]
+    },
+    {
+      name: "Weather",
+      picture: "Weather.png",
+      description: "Displays your local weather with beautiful animations.",
+      link: "weather/",
+      technologies: ["CSS3 animations", "inline SVG", "jQuery", "Geolocation"]
+    },
+    {
+      name: "Simon Game",
+      picture: "Simon.png",
+      description: "A replica of the classics from the 1970s. Feel the nostalgia!",
+      link: "simon/",
+      technologies: ["CSS3", "HTML5 Audio", "jQuery"]
+    },
+    {
+      name: "Tic-Tac-Toe",
+      picture: "Tic-tac-toe.png",
+      description: "Play against three levels of difficulty. The middle one is smart and fun to play. The hardest one is invincible!",
+      link: "tic-tac-toe/",
+      technologies: ["Artificial intelligence", "Animate.js"]
+    },
+    {
+      name: "Retro Pomodoro Clock",
+      picture: "Pomodoro.png",
+      description: "the timepiece for the office, styled like a 1990s gadget.",
+      link: "pomodoro/",
+      technologies: ["JavaScript", "CSS3", "HTML5 Audio"]
+    }
+  ];
+
+  ReactDOM.render(
+    <PortfolioList data={frontendPortfolioArray} />,
+    document.getElementById('frontend-react-container')
+  );
+
+  // Render the clock in the tablet
+
+  var Clock = React.createClass({
+    render: function() {
+      var dateHours = this.props.date.getHours();
+      var dateMinutes = this.props.date.getMinutes().toString();
+      var thisHours = dateHours < 12 ? dateHours : dateHours - 12;
+      var thisMinutes = dateMinutes.length === 2 ? dateMinutes : '0' + dateMinutes;
+      var amPm = dateHours < 12 ? 'AM' : 'PM';
+      return (
+        <span>
+          {thisHours}:{thisMinutes} {amPm}
         </span>
       );
-    });
-    return (
-      <div className="tagsDiv">
-        {tagArray}
-      </div>
+    }
+  });
+
+  setInterval(function() {
+    ReactDOM.render(
+      <Clock date={new Date()} />,
+      document.getElementById('tablet-time')
     );
-  }
-});
-
-var PortfolioItem = React.createClass({
-  handleClick: function(url) {
-    var iframe = document.getElementById('tablet-iframe');
-    iframe.src = url + 'index.html';
-    iframe.style.display = 'block';
-    window.setTimeout(function () {
-      iframe.style.opacity = '1';
-    }, 200); // Arbitrary time to load app
-  },
-  render: function() {
-    return (
-      <div className="portfolioItem" onClick={this.handleClick.bind(null, this.props.item.link)}>
-        <img src={'img/' + this.props.item.picture} />
-        <div className="portfolioItemName">
-          {this.props.item.name}
-        </div>
-      </div>
-    );
-  }
-});
-
-var PortfolioList = React.createClass({
-  render: function() {
-    var itemArray = this.props.data.map(function(item, index) {
-      return (
-        <PortfolioItem key={index} item={item} />
-      );
-    });
-    return (
-      <div className="portfolioFlexContainer">
-        {itemArray}
-      </div>
-    );
-  }
-});
-
-var frontendPortfolioArray = [
-  {
-    name: "Chocolate Calc",
-    picture: "Calculator.png",
-    description: "A calculator that looks like a chocolate bar!",
-    link: "calculator/",
-    technologies: ["CSS3", "JavaScript"]
-  },
-  {
-    name: "Weather",
-    picture: "Weather.png",
-    description: "Displays your local weather with beautiful animations.",
-    link: "weather/",
-    technologies: ["CSS3 animations", "inline SVG", "jQuery", "Geolocation"]
-  },
-  {
-    name: "Simon Game",
-    picture: "Simon.png",
-    description: "A replica of the classics from the 1970s. Feel the nostalgia!",
-    link: "simon/",
-    technologies: ["CSS3", "HTML5 Audio", "jQuery"]
-  },
-  {
-    name: "Tic-Tac-Toe",
-    picture: "Tic-tac-toe.png",
-    description: "Play against three levels of difficulty. The middle one is smart and fun to play. The hardest one is invincible!",
-    link: "tic-tac-toe/",
-    technologies: ["Artificial intelligence", "Animate.js"]
-  },
-  {
-    name: "Retro Pomodoro Clock",
-    picture: "Pomodoro.png",
-    description: "the timepiece for the office, styled like a 1990s gadget.",
-    link: "pomodoro/",
-    technologies: ["JavaScript", "CSS3", "HTML5 Audio"]
-  }
-];
-
-ReactDOM.render(
-  <PortfolioList data={frontendPortfolioArray} />,
-  document.getElementById('frontend-react-container')
-);
-
-// Render the clock in the tablet
-
-var Clock = React.createClass({
-  render: function() {
-    var dateHours = this.props.date.getHours();
-    var dateMinutes = this.props.date.getMinutes().toString();
-    var thisHours = dateHours < 12 ? dateHours : dateHours - 12;
-    var thisMinutes = dateMinutes.length === 2 ? dateMinutes : '0' + dateMinutes;
-    var amPm = dateHours < 12 ? 'AM' : 'PM';
-    return (
-      <span>
-        {thisHours}:{thisMinutes} {amPm}
-      </span>
-    );
-  }
-});
-
-setInterval(function() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('tablet-time')
-  );
-}, 1000); // Try to update every second
+  }, 1000); // Try to update every second
+})();
--- script/index-script.js
+++ script/index-script.js
@@ -1,65 +1,112 @@
-// jQuery-free logic to select tabs with the navbar
-var animationDuration = 400;
-var animationDelay = 10;
-var navLinks = document.getElementsByClassName('nav-link');
-var navTabs = document.getElementsByTagName('li');
-var sections = document.getElementsByClassName('section');
+var myPortfolio = (function () {
 
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener('click', function (e) {
-    e = e ? e : window.event; // Sounds funny
-    var activeTab = document.getElementsByClassName('active');
-    activeTab[0].className = '';
-    for (var j = 0; j < sections.length; j++) {
-      sections[j].style.opacity = '0';
-      (function (thisJ) {
-        window.setTimeout(function () {
-          sections[thisJ].style.display = 'none';
-        }, animationDuration);
-      }(j));
-    }
-    var thisSection;
-    var thisTabText = e.target ?
-      e.target.textContent.trim() : e.srcElement.textContent.trim();
-    for (var k = 0; k < navTabs.length; k++) {
-      if (navTabs[k].textContent.trim() === thisTabText) {
-        navTabs[k].className = 'active';
-        switch (thisTabText) {
-          case 'About me':
-            thisSection = document.getElementById('about-me');
-            document.documentElement.style.boxShadow =
-            'inset 0 0 20em 2em #aaa7a2';
-            document.documentElement.style.backgroundColor =
-            '#dedcd9';
-            break;
-          case 'Front-end apps':
-            thisSection = document.getElementById('front-end');
-            document.documentElement.style.boxShadow =
-            'inset 0 0 20em 2em #ca8782';
-            document.documentElement.style.backgroundColor =
-            '#febcb9';
-            break;
-        }
-        window.setTimeout(function () {
-          thisSection.style.display = 'block';
+  // jQuery-free logic to select tabs with the navbar
+  var animationDuration = 400;
+  var animationDelay = 10;
+  var navLinks = document.getElementsByClassName('nav-link');
+  var navTabs = document.getElementsByTagName('li');
+  var sections = document.getElementsByClassName('section');
+
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function (e) {
+      e = e ? e : window.event; // Sounds funny
+      var activeTab = document.getElementsByClassName('active');
+      activeTab[0].className = '';
+      for (var j = 0; j < sections.length; j++) {
+        sections[j].style.opacity = '0';
+        (function (thisJ) {
           window.setTimeout(function () {
-            thisSection.style.opacity = '1';
-          }, animationDelay);
-        }, animationDelay + animationDuration);
+            sections[thisJ].style.display = 'none';
+          }, animationDuration);
+        }(j));
       }
+      var thisSection;
+      var thisTabText = e.target ?
+        e.target.textContent.trim() : e.srcElement.textContent.trim();
+      for (var k = 0; k < navTabs.length; k++) {
+        if (navTabs[k].textContent.trim() === thisTabText) {
+          navTabs[k].className = 'active';
+          switch (thisTabText) {
+            case 'About me':
+              thisSection = document.getElementById('about-me');
+              document.documentElement.style.boxShadow =
+              'inset 0 0 20em 2em #aaa7a2';
+              document.documentElement.style.backgroundColor =
+              '#dedcd9';
+              break;
+            case 'Front-end apps':
+              thisSection = document.getElementById('front-end');
+              document.documentElement.style.boxShadow =
+              'inset 0 0 20em 2em #ca8782';
+              document.documentElement.style.backgroundColor =
+              '#febcb9';
+              break;
+          }
+          window.setTimeout(function () {
+            thisSection.style.display = 'block';
+            window.setTimeout(function () {
+              thisSection.style.opacity = '1';
+            }, animationDelay);
+          }, animationDelay + animationDuration);
+        }
+      }
+    }, false);
+  }
+
+  // Run once
+  var menuButtom = document.getElementById('tablet-menu-div');
+  var menu = document.getElementById('tablet-menu');
+  var iframe = document.getElementById('tablet-iframe');
+
+  document.getElementById('about-me').style.cssText =
+  'display: block; opacity: 1;';
+
+  // Event listeners for mouse clicks on certain elements
+  document.getElementById('tablet-button').addEventListener('click', function (e) {
+    closeTabletMenu();
+    iframe.style.opacity = menuButtom.style.opacity ='0';
+    window.setTimeout(function () {
+      iframe.style.display = menuButtom.style.display = 'none';
+      iframe.src = 'about:blank';
+    }, animationDuration);
+  }, false);
+
+  menuButtom.addEventListener('click', function (e) {
+    if (menu.style.display !== 'inline-block') {
+      openTabletMenu();
+    } else {
+      closeTabletMenu();
     }
   }, false);
-}
 
-// Run once
-document.getElementById('about-me').style.cssText =
-'display: block; opacity: 1;';
+  // Functions to animate the UI
+  var openTabletMenu = function () {
+    menu.style.display = 'inline-block';
+    window.setTimeout(function () {
+      menu.style.opacity = '1';
+    }, animationDuration / 2);
+  };
 
-document.getElementById('tablet-button').addEventListener('click', function (e) {
-  var iframe = document.getElementById('tablet-iframe');
-  iframe.style.opacity = '0';
-  window.setTimeout(function () {
-    iframe.style.display = 'none';
-    iframe.src = 'about:blank';
-  }, animationDuration);
-}, false);
+  var closeTabletMenu = function () {
+    menu.style.opacity = '0';
+    window.setTimeout(function () {
+      menu.style.display = 'none';
+    }, animationDuration / 2);
+  };
+
+  var openApp = function (url) {
+    iframe.src = url + 'index.html';
+    iframe.style.display = 'block';
+    menuButtom.style.display = 'flex';
+    window.setTimeout(function () {
+      iframe.style.opacity = menuButtom.style.opacity = '1';
+    }, 200); // Arbitrary time to load app
+  }
+
+  // Exporting the functions above to index-react.js
+  return {
+    openTabletMenu: openTabletMenu,
+    closeTabletMenu: closeTabletMenu,
+    openApp: openApp
+  }
+})();
